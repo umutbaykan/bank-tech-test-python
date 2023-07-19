f5b314f119060022d9c3c26402f76887cc55a88f
umut <umut_baykan@hotmail.com>
2023-07-19 15:54:30 +0300

    removal of user class as not required by specs

--- lib/user.py
+++
@@ -1,3 +0,0 @@
-class User:
-    def __init__(self, account=None) -> None:
-        self.account = None or account
--- tests/test_user.py
+++
@@ -1,9 +0,0 @@
-from lib.user import User
-
-
-def test_user_instance_creation():
-    """
-    Creates a user class instance with no default account
-    """
-    user = User()
-    assert user.account == None
