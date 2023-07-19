4d066c5e25a7f26458ab01956be6418f2f32320b
umut <umut_baykan@hotmail.com>
2023-07-19 16:30:49 +0300

    printer class to print logs in correct format

--- lib/printer.py
+++ lib/printer.py
@@ -1,7 +1,10 @@
 class Printer:
-    @staticmethod
     def header():
         print("date || credit || debit || balance")
 
-    def print_logs(logs):
-        Printer.header()
+    def print_formatted_log(log):
+        date = log.date.strftime("%d-%m-%Y")
+        credit = "" if log.credit == 0 else "{:.2f}".format(log.credit)
+        debit = "" if log.debit == 0 else "{:.2f}".format(log.debit)
+        balance = "{:.2f}".format(log.balance)
+        print(f"{date} || {credit} || {debit} || {balance}")
--- tests/test_printer.py
+++ tests/test_printer.py
@@ -1,12 +1,39 @@
 from lib.printer import Printer
-import unittest
 from unittest.mock import patch, Mock
+from unittest import TestCase
+from datetime import date
 
 
-def test_printer_header():
-    """
-    Tests whether statement header is printed correctly
-    """
-    with patch("builtins.print") as mock_print:
-        Printer.header()
-        mock_print.assert_called_with("date || credit || debit || balance")
+class FakeLogs(TestCase):
+    def setUp(self):
+        self.fake_log_1 = Mock()
+        self.fake_log_1.debit = 0
+        self.fake_log_1.credit = 100.50
+        self.fake_log_1.balance = 122.569
+        self.fake_log_1.date = date.fromisoformat("2000-06-05")
+
+        self.fake_log_2 = Mock()
+        self.fake_log_2.debit = 30.20
+        self.fake_log_2.credit = 0
+        self.fake_log_2.balance = 70.30
+        self.fake_log_2.date = date.fromisoformat("2000-06-09")
+
+        self.logs = [self.fake_log_1, self.fake_log_2]
+
+
+class TestPrinterClass(FakeLogs):
+    def test_printer_header(self):
+        """
+        Tests whether statement header is printed correctly
+        """
+        with patch("builtins.print") as mock_print:
+            Printer.header()
+            mock_print.assert_called_with("date || credit || debit || balance")
+
+    def test_print_formatted_log(self):
+        """
+        Tests whether the log is printed in the correct format
+        """
+        with patch("builtins.print") as mock_print:
+            Printer.print_formatted_log(self.fake_log_1)
+            mock_print.assert_called_with("05-06-2000 || 100.50 ||  || 122.57")
