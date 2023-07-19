91b8d797ae5bc4b5945f3c76bc742d46059fe075
umut <umut_baykan@hotmail.com>
2023-07-19 12:35:56 +0300

    initiating user class

---
+++ lib/user.py
@@ -0,0 +1,3 @@
+class User():
+    def __init__(self, account=None) -> None:
+        self.account = None or account 
--- tests/test_bank.py
+++
@@ -1,5 +0,0 @@
-import pytest
-from lib.bank import Bank
-
-def test_initial_launch():
-    pass
