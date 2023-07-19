befa8c343708e3a1f625762010ac5dc51f59d37c
umut <umut_baykan@hotmail.com>
2023-07-19 13:24:04 +0300

    account class deposit method

--- lib/account.py
+++ lib/account.py
@@ -1,4 +1,13 @@
+from .log import Log
+from datetime import datetime
+
+
 class Account:
-    def __init__(self, balance=0, logs=[]) -> None:
+    def __init__(self, balance=0, logs=[], logger=Log) -> None:
         self.balance = 0 or balance
+        self.logger = logger
         self.logs = logs if logs is not None else []
+
+    def deposit(self, amount, date=datetime.today()):
+        log = self.logger(credit=amount, date=date)
+        self.logs.append(log)
--- tests/test_account.py
+++ tests/test_account.py
@@ -1,4 +1,5 @@
 from lib.account import Account
+from datetime import date
 
 
 def test_account_creation():
@@ -16,3 +17,14 @@ def test_account_balance_override():
     """
     account = Account(balance=100)
     assert account.balance == 100
+
+
+def test_account_deposit_amount():
+    """
+    Creates and adds a log object to the account logs.
+    """
+    account = Account()
+    fake_date = date.fromisoformat("2000-06-05")
+    account.deposit(100, date=fake_date)
+    assert len(account.logs) == 1
+    assert account.logs[0].date == fake_date
